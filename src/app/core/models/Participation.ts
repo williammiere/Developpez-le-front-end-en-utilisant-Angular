db25ea21884ce4a4417cf99a5e81253c59a328d8
Andrea Franco <dedefac96@gmail.com>
2024-06-28 11:19:11 +0200

    Created the Olympic and Participation interfaces

--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -1,11 +1,7 @@
-// TODO: create here a typescript interface for a participation
-/*
-example of participation:
-{
-    id: 1,
-    year: 2012,
-    city: "Londres",
-    medalsCount: 28,
-    athleteCount: 372
+export interface Participation {
+    id: string;
+    year: string;
+    city: string;
+    medalCount: number;
+    athleteCount: number;
 }
-*/
