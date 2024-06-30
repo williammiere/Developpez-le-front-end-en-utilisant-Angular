e908f47fae5cfd30643a7a581c41467d46a4d623
Andrea Franco <dedefac96@gmail.com>
2024-06-30 16:51:56 +0200

    Implemented the first draft of home page

---
+++ src/app/core/models/PieChartData.ts
@@ -0,0 +1,3 @@
+export class PieChartData {
+    constructor(public name: string, public value: number){}
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
 import { Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { PieChartData } from 'src/app/core/models/PieChartData';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -10,10 +12,56 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[]> = of([]);
+  public pieChartDataList: PieChartData[] = [];
+  public olympicCount: number = 0;
+  public countryCount: number = 0;
+
+  view: any = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: string = 'below';
+
+  colorScheme: Color = { 
+    domain: ['#793d52', '#89a1db', '#9780a1', '#bfe0f1', '#b8cbe7', '#956065'], 
+    group: ScaleType.Ordinal, 
+    selectable: true, 
+    name: 'Customer Usage', 
+};
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe( olympics => {
+
+        this.countryCount = olympics.length;
+        this.olympicCount = this.getOlympicCount(olympics);
+
+        olympics.forEach(olympic => {
+        this.pieChartDataList.push(new PieChartData(olympic.country, this.getMedalCountByOlympic(olympic)));
+      });
+    });
+  }
+
+  getMedalCountByOlympic(olympic : Olympic): number {
+    return olympic.participations.reduce((medalCount, participation) => { return medalCount + participation.medalsCount; }, 0);
+  }
+
+  getOlympicCount(olympics : Olympic[]): number {
+    let cities : Set<string> = new Set();
+    olympics.forEach(olympic => {
+      olympic.participations.forEach(participation => {
+        cities.add(participation.city);
+      });
+    });
+    return cities.size;
+  }
+
+  onSelect(data: PieChartData): void {
+
   }
 }
