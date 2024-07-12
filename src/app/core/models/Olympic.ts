1a98c06961a27ac99026b3931a6459235b9de613
SivaPrinda <prinda.sivakumaran@gmail.com>
2024-07-12 16:27:07 +0200

    modification of model type

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,18 +1,7 @@
-// TODO: create here a typescript interface for an olympic country
-
 import { Participation } from './Participation';
 
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
-}
-*/
-export class Olympic {
+export interface Olympic {
   id: string;
-  constructor(public country: string, public participations: Participation[]) {
-    this.id = crypto.randomUUID().substring(0, 8);
-  }
+  country: string;
+  participations: Participation[];
 }
--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -1,22 +1,7 @@
-// TODO: create here a typescript interface for a participation
-/*
-example of participation:
-{
-    id: 1,
-    year: 2012,
-    city: "Londres",
-    medalsCount: 28,
-    athleteCount: 372
-}
-*/
-export class Participation {
+export interface Participation {
   id: string;
-  constructor(
-    public year: number,
-    public city: string,
-    public medalsCount: number,
-    public athleteCount: number
-  ) {
-    this.id = crypto.randomUUID().substring(0, 8);
-  }
+  year: number;
+  city: string;
+  medalsCount: number;
+  athleteCount: number;
 }
