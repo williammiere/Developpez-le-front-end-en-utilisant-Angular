25182576539ef919cdf87f264f54a98813e30f0d
Torus-Ortie <velez.romain@orange.fr>
2024-08-27 17:35:08 +0200

    Remove all "any" type

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { OlympicCountry } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,11 +9,22 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<OlympicCountry[]> = of([]);
+  private totalOfMedal: number = 0;
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+
+    this.olympics$.forEach(olympicCountries => {
+      olympicCountries.forEach(olympicCountry => {
+        for (let index = 0; index < olympicCountry.participations.length; index++) {
+          this.totalOfMedal += olympicCountry.participations[index].medalsCount;
+        }
+        olympicCountry.totalOfMedal = this.totalOfMedal;
+        this.totalOfMedal = 0;
+      });
+    });
   }
 }
